dd9e6568135d716e67cb4bc211f7235f9ef39ad2
Cole Gartner <cgartner37@gmail.com>
2022-03-24 11:46:01 -0500

    Finished tour-of-heroes

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,8 +11,10 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
+  displayHeroes: Hero[] = [];
+  amount: string = "5";
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private mssgService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -19,7 +22,21 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(0, 5));
+      .subscribe(heroes => this.heroes = heroes);
+    this.updateList();
+  }
+
+  updateList(): void {
+    let topAmt: number = parseInt(this.amount);
+    if (!(topAmt > 0)) {
+      topAmt = 5;
+    }
+    this.mssgService.add("Got top " + topAmt + " Heroes");
+    this.displayHeroes = this.heroes.slice(0, topAmt);
+  }
+
+  getDisplayHeroes(): Hero[] {
+    return this.displayHeroes;
   }
 
 }
